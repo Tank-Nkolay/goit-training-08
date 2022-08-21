75ee2007b3b9237550a01fb1975415ad20dea698
Nikolay Tankov <tankov.n.k@gmail.com>
2022-08-21 13:47:48 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,14 +4,33 @@
 
 // app.set('view engine', 'hbs');
 
-import pokemonCardTpl from './';
+// import pokemonCardTpl from './';
 // import q from '../../templat';
 
-fetch('https://pokeapi.co/api/v2/pokemon/2')
+const qqq = document.querySelector('.qqq');
+const www = document.querySelector('.form');
+let onForm = 1;
+
+www.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  onForm = form.elements.query.value;
+  return console.log(onForm);
+}
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${onForm}`)
   .then(response => {
     return response.json();
   })
   .then(pokemon => {
+    const x = `
+    <p>Имя: ${pokemon.name} </p>
+    <img src = "${pokemon.sprites.back_default}" alt = "${pokemon.name}" width="200" height="200">
+    <p>Вес: ${pokemon.weight} </p>
+    <p>Рост: ${pokemon.height} </p>`;
+    qqq.insertAdjacentHTML('beforeend', x);
     console.log(pokemon);
   })
   .catch(error => {
