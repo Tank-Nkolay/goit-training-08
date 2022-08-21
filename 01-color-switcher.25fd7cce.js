e9d6c2a15e03272ed8703721c4dd231b28ff04e6
Tank-Nkolay <99677103+Tank-Nkolay@users.noreply.github.com>
2022-08-21 10:48:20 +0000

    Deploying to gh-pages from @ Tank-Nkolay/goit-training-08@75ee2007b3b9237550a01fb1975415ad20dea698 🚀

--- 01-color-switcher.25fd7cce.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pokeapi.co/api/v2/pokemon/2").then((function(o){return o.json()})).then((function(o){console.log(o)})).catch((function(o){console.log(o)}));
-//# sourceMappingURL=01-color-switcher.25fd7cce.js.map
--- 01-color-switcher.df6ab236.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pokeapi.co/api/v2/pokemon/2").then((o=>o.json())).then((o=>{console.log(o)})).catch((o=>{console.log(o)}));
-//# sourceMappingURL=01-color-switcher.df6ab236.js.map
