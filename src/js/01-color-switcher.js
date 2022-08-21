7d9d5e4a6a7b67839cc87232d452ad55fdecd798
Nikolay Tankov <tankov.n.k@gmail.com>
2022-08-21 13:59:14 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,30 +9,30 @@
 
 const qqq = document.querySelector('.qqq');
 const www = document.querySelector('.form');
-let onForm = 1;
+let onForm;
 
 www.addEventListener('submit', onSearch);
 
 function onSearch(e) {
   e.preventDefault();
+
   const form = e.currentTarget;
   onForm = form.elements.query.value;
-  return console.log(onForm);
-}
-
-fetch(`https://pokeapi.co/api/v2/pokemon/${onForm}`)
-  .then(response => {
-    return response.json();
-  })
-  .then(pokemon => {
-    const x = `
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${onForm}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(pokemon => {
+      const x = `
     <p>Имя: ${pokemon.name} </p>
-    <img src = "${pokemon.sprites.back_default}" alt = "${pokemon.name}" width="200" height="200">
+    <img src = "${pokemon.sprites.front_default}" alt = "${pokemon.name}" width="200" height="200">
     <p>Вес: ${pokemon.weight} </p>
     <p>Рост: ${pokemon.height} </p>`;
-    qqq.insertAdjacentHTML('beforeend', x);
-    console.log(pokemon);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+      qqq.insertAdjacentHTML('beforeend', x);
+      console.log(pokemon);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => www.reset());
+}
