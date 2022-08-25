8897f827b1db5db542ff58f493806df60a8b6e00
Tank-Nkolay <99677103+Tank-Nkolay@users.noreply.github.com>
2022-08-25 05:25:16 +0000

    Deploying to gh-pages from @ Tank-Nkolay/goit-training-08@d61d5f9e44b82b938f894fc9a18423e8550d9860 🚀

---
+++ 01-color-switcher.5b4d9327.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/books").then((function(o){return o.json()})).then(console.log);
+//# sourceMappingURL=01-color-switcher.5b4d9327.js.map
---
+++ 01-color-switcher.e9d14233.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/books").then((o=>o.json())).then(console.log);
+//# sourceMappingURL=01-color-switcher.e9d14233.js.map
