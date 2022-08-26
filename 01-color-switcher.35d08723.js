124ed49b3a5e2c5fcdf74820be9b77c60111fd64
Tank-Nkolay <99677103+Tank-Nkolay@users.noreply.github.com>
2022-08-26 17:51:15 +0000

    Deploying to gh-pages from @ Tank-Nkolay/goit-training-08@9f0387881c9a433d6de479a47c8e9edc264a83c6 🚀

--- 01-color-switcher.35d08723.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,n="http://localhost:3000";o=11,fetch("".concat(n,"/books/").concat(o),{method:"DELETE"}).then((function(o){return o.json()})).then(console.log),fetch("".concat(n,"/books")).then((function(o){return o.json()})).then(console.log)}();
-//# sourceMappingURL=01-color-switcher.35d08723.js.map
---
+++ 01-color-switcher.e3d9ce77.js
@@ -0,0 +1,2 @@
+fetch(`http://localhost:3000/books/${11}`,{method:"DELETE"}).then((o=>o.json())).then(console.log).catch((o=>console.log(o))),fetch("http://localhost:3000/books").then((o=>o.json())).then(console.log);
+//# sourceMappingURL=01-color-switcher.e3d9ce77.js.map
