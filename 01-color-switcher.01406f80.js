a68f4f9f2ded869a167d814966211074686364bb
Tank-Nkolay <99677103+Tank-Nkolay@users.noreply.github.com>
2022-08-27 03:54:11 +0000

    Deploying to gh-pages from @ Tank-Nkolay/goit-training-08@b2fd50a81032af3159f48ebb3c629ed2632536ac 🚀

---
+++ 01-color-switcher.01406f80.js
@@ -0,0 +1,2 @@
+!function(){try{console.log("внутри try")}catch(o){console.log("Ошибка!")}console.log("После try ... catch")}();
+//# sourceMappingURL=01-color-switcher.01406f80.js.map
---
+++ 01-color-switcher.67aa4f9f.js
@@ -0,0 +1,2 @@
+try{console.log("внутри try")}catch(o){console.log("Ошибка!")}console.log("После try ... catch");
+//# sourceMappingURL=01-color-switcher.67aa4f9f.js.map
