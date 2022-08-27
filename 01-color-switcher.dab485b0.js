d9f84e93b853f0c23a6d0c8eeba702c19b07de25
Tank-Nkolay <99677103+Tank-Nkolay@users.noreply.github.com>
2022-08-27 06:59:40 +0000

    Deploying to gh-pages from @ Tank-Nkolay/goit-training-08@552351a2e454088408a76210af3e086af9ffe3a7 🚀

--- 01-color-switcher.dab485b0.js
+++
@@ -1,2 +0,0 @@
-function o(o){const e={strawberry:"Клубника",kiwi:"Киви",apple:"Яблоко"};return new Promise((t=>setTimeout((()=>t(e[o])),500)))}!async function(){try{console.time("aMakeSmoothie");const e=o("apple"),t=o("kiwi"),i=o("strawberry"),n=await Promise.all([e,t,i]);console.log(n),console.timeEnd("aMakeSmoothie")}catch(o){console.log(o)}}();
-//# sourceMappingURL=01-color-switcher.dab485b0.js.map
