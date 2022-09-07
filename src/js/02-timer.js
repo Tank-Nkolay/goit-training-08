097a64249addd6f623520c59ff483b2fa313b077
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-07 08:01:23 +0300

    1

---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+// слушаем и получаем данные от формы ввода,списка и кнопок
+const refs = {
+  form: document.querySelector('.form'),
+  list: document.querySelector('.list'),
+  buttons: document.querySelector('.buttons'),
+};
