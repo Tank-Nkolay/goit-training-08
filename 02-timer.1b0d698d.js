4667a32a5419c7427965e9c47f3432f947335994
Tank-Nkolay <99677103+Tank-Nkolay@users.noreply.github.com>
2022-09-07 05:01:54 +0000

    Deploying to gh-pages from @ Tank-Nkolay/goit-training-08@097a64249addd6f623520c59ff483b2fa313b077 🚀

---
+++ 02-timer.1b0d698d.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),document.querySelector(".list"),document.querySelector(".buttons");
+//# sourceMappingURL=02-timer.1b0d698d.js.map
---
+++ 02-timer.ebca1204.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),document.querySelector(".list"),document.querySelector(".buttons");
+//# sourceMappingURL=02-timer.ebca1204.js.map
