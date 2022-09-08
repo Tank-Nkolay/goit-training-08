b07d33f23195e0e8d73f8f031ecd347a0f11921f
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-08 09:01:58 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// дефолтный URL, он вставляется в строку запроса на сервер
+axios.defaults.baseURL = 'https://hn.algolia.com/api/v1';
+
 // слушаем и получаем данные от формы ввода,списка и кнопок
 const refs = {
   form: document.querySelector('.form'),
@@ -8,18 +11,39 @@ const refs = {
 };
 
 let query = '';
+let page = 0;
+let items = [];
+// количество всех страниц которые нашли по запросу
+let totalPages = 0;
+
+// отдельная функция под items в функции fetchData ===========
+const render = () => {
+  console.log(items);
+};
+
+// функция запроса на сервер =========================
+const fetchData = () => {
+  axios
+    .get(`/search?query=${query}&page=${page}`)
+    .then(({ data }) => {
+      // записываем hits, определив что это наши данные из масива
+      items = data.hits;
+      totalPages = data.nbPages;
+      console.log(data);
+      console.log(totalPages);
+      render();
+    })
+    .catch(error => console.log(error.message));
+};
 
 // подписываемся на действие САБМИТ
 const handleSubmit = e => {
   e.preventDefault();
-
   // получаем данные из САБМИТ формы после отправки
   query = e.target.elements.query.value;
   console.log(query);
+  // выполняем функцию и получаем масив(страница) данных
+  fetchData();
 };
 
 refs.form.addEventListener('submit', handleSubmit);
-
-// функция запроса на сервер
-
-const fetchData = () => {};
